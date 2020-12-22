b42dc1f14f9cfdcb304f367387d440baf65f728e
liuguanqiang <liuguanqiang@hetao101.com>
2020-12-22 17:51:53 +0800

    新增两个关卡

--- typing/main.js
+++ typing/main.js
@@ -135,7 +135,7 @@ window.boot = function () {
         BK.Script.loadlib();
     }
     else {
-        var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.js?v=2020070101';
+        var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.js?v=2020121801';
         if (jsList) {
             jsList = jsList.map(function (x) {
                 return 'src/' + x;
